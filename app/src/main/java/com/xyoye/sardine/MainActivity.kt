6650b4cf0f8dc12883c33f1692db408e4c0096bc
xyoye <xyoye1997@outlook.com>
2023-01-08 16:28:11 +0800

    [新增功能](master): 完善Demo

--- app/src/main/java/com/xyoye/sardine/MainActivity.kt
+++ app/src/main/java/com/xyoye/sardine/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.xyoye.sardine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.xyoye.sardine.impl.OkHttpSardine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val sardine = OkHttpSardine(UnsafeOkHttpClient.client)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        tvResult.movementMethod = ScrollingMovementMethod.getInstance()
+
+        val etUrl = findViewById<EditText>(R.id.et_url)
+        val etAccount = findViewById<EditText>(R.id.et_account)
+        val etPwd = findViewById<EditText>(R.id.et_pwd)
+
+        findViewById<Button>(R.id.bt_connect).setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val result = testConnect(
+                    etUrl.text?.toString(),
+                    etAccount.text?.toString(),
+                    etPwd.text?.toString()
+                )
+                withContext(Dispatchers.Main) {
+                    tvResult.text = result
+                }
+            }
+        }
     }
+
+    private suspend fun testConnect(
+        url: String?,
+        account: String?,
+        pwd: String?
+    ): String {
+        if (TextUtils.isEmpty(url)) {
+            tips("url不能为空")
+            return ""
+        }
+        if (TextUtils.isEmpty(account).not() && TextUtils.isEmpty(pwd).not()) {
+            sardine.setCredentials(account, pwd)
+        }
+
+        try {
+            return sardine.list(url).joinToString(separator = "\n") { it.name }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            tips(e.message ?: "打开目录失败")
+        }
+        return ""
+    }
+
+    private suspend fun tips(msg: String) {
+        withContext(Dispatchers.Main) {
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/xyoye/sardine/UnsafeOkHttpClient.kt
@@ -0,0 +1,49 @@
+package com.xyoye.sardine
+
+import android.annotation.SuppressLint
+import okhttp3.OkHttpClient
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
+
+/**
+ * Created by xyoye on 2021/5/2.
+ *
+ * 忽略证书验证的OkHttpClient
+ */
+
+object UnsafeOkHttpClient {
+
+    private val unSafeTrustManager = object : X509TrustManager {
+        @SuppressLint("TrustAllX509TrustManager")
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        @SuppressLint("TrustAllX509TrustManager")
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+    }
+
+    private val sslContext = SSLContext.getInstance("TLS").apply {
+        init(null, arrayOf(unSafeTrustManager), null)
+    }
+
+    val client: OkHttpClient by lazy {
+        val baseClient: OkHttpClient = OkHttpClient.Builder()
+            .followRedirects(false)
+            .followSslRedirects(false)
+            .retryOnConnectionFailure(false)
+            .build()
+        baseClient.newBuilder()
+            .sslSocketFactory(sslContext.socketFactory, unSafeTrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+    }
+}
